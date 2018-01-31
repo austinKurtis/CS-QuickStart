bdc7a35fc00837ec0c9ac50b0a22be10b8d29612
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-31 11:48:23 -0600

    Do more with strings

--- DeclareVariables/Program.cs
+++ DeclareVariables/Program.cs
@@ -20,6 +20,29 @@ namespace DeclareVariables
             Console.WriteLine($"My friends are {firstFriend} and {secondFriend}.");
             Console.WriteLine($"The name of {firstFriend} has {firstFriend.Length} letters.");
             Console.WriteLine($"The name of {secondFriend} has {secondFriend.Length} letters.");
+
+            string message = "        Hello World!         ";
+            Console.WriteLine($"[{message}]");
+
+            string trimmedMessage = message.TrimStart();
+            Console.WriteLine($"[{trimmedMessage}]");
+
+            trimmedMessage = message.TrimEnd();
+            Console.WriteLine($"[{trimmedMessage}]");
+
+            trimmedMessage = message.Trim();
+            Console.WriteLine($"[{trimmedMessage}]");
+
+            string messageHW = "Hello World!";
+            Console.WriteLine(messageHW);
+
+            messageHW = messageHW.Replace("Hello", "Greetings");
+            Console.WriteLine(messageHW);
+
+            string whatUp = "What Up, Yo!";
+            Console.WriteLine(whatUp);
+            Console.WriteLine(whatUp.ToUpper());
+            Console.WriteLine(whatUp.ToLower());
         }
     }
 }
