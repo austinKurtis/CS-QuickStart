8d8c73ac63446ee65a7aebf92461ab4441109a02
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-31 10:50:43 -0600

    first commit

---
+++ DeclareVariables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DeclareVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
