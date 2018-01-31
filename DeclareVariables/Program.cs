d0c1337bda289098b17b509b8a2b8b6dd2388958
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-31 10:57:19 -0600

    Working with Strings

--- DeclareVariables/Program.cs
+++ DeclareVariables/Program.cs
@@ -13,6 +13,13 @@ namespace DeclareVariables
             Console.WriteLine("Hello " + aFriend);
 
             Console.WriteLine($"Hello {aFriend}");
+
+            string firstFriend = "Maria";
+            string secondFriend = "Sage";
+
+            Console.WriteLine($"My friends are {firstFriend} and {secondFriend}.");
+            Console.WriteLine($"The name of {firstFriend} has {firstFriend.Length} letters.");
+            Console.WriteLine($"The name of {secondFriend} has {secondFriend.Length} letters.");
         }
     }
 }
